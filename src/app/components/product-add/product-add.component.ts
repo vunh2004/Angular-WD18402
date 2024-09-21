9186354069e3dc2eab77f6e09b4de294724d30cc
NguyenHoangVu-2004 <vunhph45875@fpt.edu.vn>
2024-09-21 15:18:44 +0700

    Buổi 6

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,16 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-product-add',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './product-add.component.html',
   styleUrl: './product-add.component.css',
 })
 export class ProductAddComponent {
   @Output() onAdd = new EventEmitter<any>();
-  onSubmit(formAdd: NgForm) {
-    // gửi dữ liệu lên component cha
-    this.onAdd.emit(formAdd.value);
+  // onSubmit(formAdd: NgForm) {
+  //   // gửi dữ liệu lên component cha
+  //   this.onAdd.emit(formAdd.value);
+  // }
+  onHandleAdd(form: NgForm) {
+    if (!form.valid) return;
+    console.log(form.value);
   }
 }
